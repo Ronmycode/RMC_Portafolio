3604ad78b106b2342b8faf0d60d4f43c97d00d9e
RonMyCode <67519456+Ronmycode@users.noreply.github.com>
2024-05-04 21:58:30 -0600

    done with navbar and social icons

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -1,9 +1,11 @@
+import { Home } from "./Components/Home";
 import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 }
