80ca083ad60b18457e302c1fe5316fadd0e3fff7
RonMyCode <67519456+Ronmycode@users.noreply.github.com>
2024-05-20 16:02:09 -0600

    all up until work part

--- portafolio/src/App.js
+++ portafolio/src/App.js
@@ -1,11 +1,17 @@
+import About from "./Components/About";
 import { Home } from "./Components/Home";
 import NavBar from "./Components/NavBar";
+import Skills from "./Components/Skills";
+import Work from "./Components/Work";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
+      <Skills />
+      <Work />
     </div>
   );
 }
